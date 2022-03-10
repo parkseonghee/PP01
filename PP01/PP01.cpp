51c1a28cd0b39390048211650620446011fc87c9
성희 박 <aprk5678@naver.com>
2022-03-10 17:56:35 +0900

    [ES] 첫

---
+++ PP01/PP01.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 3, b = 5;
+    //std::cout << "Hello World!\n";
+    cout << a + b << "Hello World!\n";
+    return 0;
+}
+
